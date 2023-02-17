eb40c895d6847d810acfb6f7206d6e682a57b0f2
Angelien Gomez <56393601+angeliengomez@users.noreply.github.com>
2023-02-17 15:37:14 +0800

    Create 1. item1.php

---
+++ Refactoring/1. item1.php
@@ -0,0 +1,12 @@
+<?php
+  
+//To get the area and volume of the rectangle these are the elements: height, width, depth
+public function getProperties(array $elements, int $value ){
+$targets = ['height', 'weight', 'depth'];
+foreach ($elements as $element )
+if(in_array($targets, $element){
+return $area = $height * $width || $volume = $height * $width * $depth;
+}
+return $value;
+}
+  ?>
